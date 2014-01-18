9784a83c90b0e8616032d7f8b64cfedcee7a0241
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2014-01-18 15:43:25 -0800

    (maintenance) update project to use GruntJS

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+        // Project settings
+        koans: {
+            // Configurable paths
+            app: 'app'
+        },
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            js: {
+                files: ['<%= koans.app %>/koans/*.js'],
+                options: {
+                    livereload: true
+                }
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            }
+        },
+
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                // Change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.tmp',
+                        '<%= koans.app %>'
+                    ]
+                }
+            },
+            test: {
+                options: {
+                    port: 9001,
+                    base: [
+                        '.tmp',
+                        'test',
+                        '<%= koans.app %>'
+                    ]
+                }
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            server: '.tmp'
+        }
+    });
+
+    grunt.registerTask('server', function (target) {
+        grunt.task.run([
+            'clean:server',
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('test', function(target) {
+        if (target !== 'watch') {
+            grunt.task.run([
+                'clean:server',
+                'concurrent:test'
+            ]);
+        }
+
+        grunt.task.run([
+            'connect:test'
+        ]);
+    });
+};
